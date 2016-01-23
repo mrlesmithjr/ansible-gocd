82194f2ca8b1370dcbe7a180fec4efc5ec19b328
Larry Smith Jr <mrlesmithjr@gmail.coml>
2016-01-23 01:48:25 -0500

    Added add'l tasks for new agents
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.coml>

--- provision.sh
+++ provision.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install -y git python-pip python-dev
 sudo pip install jinja2
 sudo pip install ansible
-sudo ansible-galaxy install -r /vagrant/requirements.yml -f
-ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
+#sudo ansible-galaxy install -r /vagrant/requirements.yml -f
+#ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
